f1118387ff3c7ac6c2c93ef0caa5240936f2fb99
Aneesh Sebastian <aneeshsebastian@gmail.com>
2021-01-03 12:11:14 +0000

    My first python program

---
+++ python_basic.py
@@ -0,0 +1,14 @@
+print("Hello")
+x1 = 2
+x2 = "2"
+print(x2+x2)
+#print(x1+x2)
+value = "hello"
+print(6*value)
+print(type(value))
+principal_amount =  5000;
+interest_rate = 5.5
+num_of_years = 5
+total_amount = principal_amount * (1 + interest_rate / 100) ** num_of_years
+print(f"Total amount after {num_of_years} is {total_amount}")
+print(f"Amount after formatting is {total_amount:.2}")
